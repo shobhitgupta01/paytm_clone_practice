456cb35956050bf4e74626ff1e2eaa7f2ffe9179
Shobhit Gupta <shobhit.gupta1995@gmail.com>
2024-01-26 22:59:55 +0530

    Added sign in page

--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -5,13 +5,13 @@ export default function SignUp(){
                 <div className="text-3xl font-bold text-center mb-3">Sign Up</div>
                 <div className="text-base text-gray-500 mb-8">Enter your information to create an account</div>
                 <div className="text-lg font-semibold mb-2">First Name</div>
-                <div className="mb-6"><input className="border border-slate-300 rounded-md w-full h-9" type="text"/></div>
+                <div className="mb-6"><input className="border border-slate-300 text-gray-500 rounded-md w-full h-9 p-3" type="text"/></div>
                 <div className="text-lg font-semibold mb-2">Last Name</div>
-                <div className="mb-6"><input className="border border-slate-300 rounded-md w-full h-9" type="text"/></div>
+                <div className="mb-6"><input className="border border-slate-300 text-gray-500 rounded-md w-full h-9 p-3" type="text"/></div>
                 <div className="text-lg font-semibold mb-2">Email</div>
-                <div className="mb-6"><input className="border border-slate-300 rounded-md w-full h-9" type="text"/></div>
+                <div className="mb-6"><input className="border border-slate-300 text-gray-500 rounded-md w-full h-9 p-3" type="text"/></div>
                 <div className="text-lg font-semibold mb-2">Password</div>
-                <div className="mb-6"><input className="border border-slate-300 rounded-md w-full h-9" type="password"/></div>
+                <div className="mb-6"><input className="border border-slate-300 text-gray-500 rounded-md w-full h-9 p-3" type="password"/></div>
                 <button className="bg-black text-white w-full h-10 rounded-md p-2 mb-6">Sign Up</button>
                 <div className="text-center">Already have an account? <span className="underline cursor-pointer">Login</span></div>
 
