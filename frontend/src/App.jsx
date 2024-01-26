43161aa03328e8711a9894503f15272e08d87093
Shobhit Gupta <shobhit.gupta1995@gmail.com>
2024-01-26 22:53:28 +0530

    Added signup page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,24 @@
+import React, { Suspense } from "react";
+import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
+
+const SignIn = React.lazy(()=>import("./components/SignIn"));
+const SignUp = React.lazy(()=>import("./components/SignUp"));
+const Dashboard = React.lazy(()=>import("./components/Dashboard"));
+
+
 
 function App() {
 
   return (
-    <div>
-        Hello world
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Navigate to="/signup" />}/>
+      <Route path="signup" element={<Suspense fallback={'loading...'}><SignUp/></Suspense>}/>
+      <Route path="signin" element={<Suspense fallback={'loading...'}><SignIn/></Suspense>}/>
+      <Route path="dashboard" element={<Suspense fallback={'loading...'}><Dashboard /></Suspense>}/>
+    </Routes>
+    </BrowserRouter>
+    
   )
 }
 
---
+++ frontend/src/components/SignUp.jsx
@@ -0,0 +1,23 @@
+export default function SignUp(){
+    return(
+        <div className="bg-neutral-400 h-screen flex justify-center">
+            <div className="bg-white mt-20 h-fit p-10 rounded-lg ">
+                <div className="text-3xl font-bold text-center mb-3">Sign Up</div>
+                <div className="text-base text-gray-500 mb-8">Enter your information to create an account</div>
+                <div className="text-lg font-semibold mb-2">First Name</div>
+                <div className="mb-6"><input className="border border-slate-300 rounded-md w-full h-9" type="text"/></div>
+                <div className="text-lg font-semibold mb-2">Last Name</div>
+                <div className="mb-6"><input className="border border-slate-300 rounded-md w-full h-9" type="text"/></div>
+                <div className="text-lg font-semibold mb-2">Email</div>
+                <div className="mb-6"><input className="border border-slate-300 rounded-md w-full h-9" type="text"/></div>
+                <div className="text-lg font-semibold mb-2">Password</div>
+                <div className="mb-6"><input className="border border-slate-300 rounded-md w-full h-9" type="password"/></div>
+                <button className="bg-black text-white w-full h-10 rounded-md p-2 mb-6">Sign Up</button>
+                <div className="text-center">Already have an account? <span className="underline cursor-pointer">Login</span></div>
+
+                
+            </div>
+            
+        </div>
+    )
+}
