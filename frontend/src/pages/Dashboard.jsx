c0fa187c81fa56d0ef077d71dd55f032ec5e8bec
Shobhit Gupta <shobhit.gupta1995@gmail.com>
2024-01-28 16:23:00 +0530

    connected balance to api

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -2,20 +2,34 @@ import { useNavigate } from "react-router-dom";
 import Balance from "../components/Balance";
 import Navbar from "../components/NavBar";
 import Users from "../components/Users";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Dashboard() {
   const navigate = useNavigate()
+  const [balance, setBalance] = useState(0);
+
+  async function fetchBalance(){
+    const url = "http://localhost:3000/api/v1/account/balance";
+    const response = await axios.get(url, {
+      "headers":{
+        "Authorization" : `Bearer ${localStorage.getItem('token')}`
+      }
+    });
+    setBalance(response.data.balance);
+  }
+
   useEffect(()=>{
     if (!localStorage.getItem('token')){
       navigate('/');
     }
+    fetchBalance();
   },[])
   
   return (
     <div>
       <Navbar />
-      <Balance value={5000}/>
+      <Balance value={balance}/>
       <Users/>
     </div>
   );
